a1c5dd2af97b7a70cb53bc16c4e609ccaa77fc93
Querijn Heijmans <querijn@irule.at>
2019-06-29 13:20:08 +0200

    :electric_plug: Fix several issues and add debug logging (#15)
    * Added some clear error messages, made sure application runs only once.
    
    * Minor style changes
    
    * Added support for Garena client, and renamed Utils to LeagueMonitor for slight clarity
    
    * Fix error where League client has to be running, plus make the popups less obnoxious
    
    * Added option to start on boot.
    
    - Startup message is surpressed when the setting is on.
    - Checks whether the app is moved if the setting is on, and sets the new app location as the startup application
    
    * Most minor nitpick ever
    
    * 🎉 Version bump to 1.1.0
    
    * Fix issue with people unable to connect to League of Legends because of an SSL issue
    
    * Prevent undefined behaviour when application isn't running as an administrator
    
    * Catch issues connecting to the League Client
    
    * Add all debug logging present
    
    * Remove unused file reference
    
    * Remove another reference to app manifest
    
    * Remove ancient files
    
    * Request admin when command line is null
    
    * Improve SSL checking

---
+++ conduit/Administrator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace Conduit
+{
+    public static class Administrator
+    {
+        public static bool IsAdmin()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        public static void Elevate()
+        {
+            var currentProcessInfo = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                WorkingDirectory = Environment.CurrentDirectory,
+                FileName = Assembly.GetEntryAssembly().Location,
+                Verb = "runas"
+            };
+
+            Process.Start(currentProcessInfo);
+            Environment.Exit(0);
+        }
+    }
+}
