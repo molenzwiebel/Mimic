3e8db7ff5881d53ba0b6189cf4cfa10d21b02808
molenzwiebel <info@thijsmolendijk.nl>
2019-06-30 12:24:32 +0200

    :iphone: Add icon for no bans

--- web/src/components/champ-select/timer.ts
+++ web/src/components/champ-select/timer.ts
@@ -79,8 +79,10 @@ export default class Timer extends Vue {
      * @returns the path to the icon for the specified champion id
      */
     getChampionIcon(id: number) {
-        if (!this.$parent.championDetails[id]) return "";
+        // -1 is the no-ban icon. It is not included in ddragon.
+        if (id === -1) return "https://raw.communitydragon.org/8.1/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png";
 
+        if (!this.$parent.championDetails[id]) return "";
         return "https://ddragon.leagueoflegends.com/cdn/" + ddragon() + "/img/champion/" + this.$parent.championDetails[id].id + ".png";
     }
 
